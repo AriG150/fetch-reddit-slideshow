85619ca52e95f07457a51a2081a1172c2413d8fa
Ari <gryphon150@gmail.com>
2019-09-26 09:57:59 -0700

    updated solution. Reference teachers code to change variables. Check logic-are all DOM's called at appropriate places

---
+++ main.js
@@ -0,0 +1,55 @@
+console.log("hello")
+// create AJAX request -DONE
+//create an array of image URL -DONE
+//make form/title/description hide
+//cycle through images
+    //set interval
+    // either add images, or change the src of a single image tag
+    // button to stop the animation
+        //clearInterval
+
+//To Do 
+//line 32- all url in ajax'ed page
+// event listener for id "searchInput" -DONE
+    //create clobal function calling searchInput  
+    //insert result into ajax fetch-ing
+//feed urlimage into html div 
+//interval
+//global variable to save all thumbnails 
+
+//DOM references
+var form = document.getElementById("form");
+var submitButton = document.getElementById("submit");
+var clearButton = document.getElementById("clear");
+var inputText = form.searchInput.value;
+
+
+function settingImage(){
+    submitButton.addEventListener("submit", function(e){ 
+        e.preventDefault();
+        inputText = form.searchInput.value;
+        fetch(`http://www.reddit.com/search.json?q=${inputText}+url:.jpg+nsfw:no`)
+            .then(function(randomData){
+                return randomData.json()
+            })
+            .then(function(jsonImage){
+                var thumbnails = jsonImage.data.children.map(function(picture){
+                    if(picture.data.url.inclues('jpg') || picture.data.url.inclues('png')) {
+                        return picture.data.url
+                    }
+                    
+                })  
+                thumbnails = thumbnails.filter(function(item){
+                    return typeof item !== 'undefined';
+                })
+                show.src = thumbnails[0];
+                handle = setInterval(function(){
+                    if(imageIndex >= thumbnails.length){
+                        imageIndex = -1;
+                    }
+                    imageIndex++;
+                    SHOW.src = thumbnails[IMAGEINDEX]; //SHOW and IMAGEINDEX are Steve's variables. Adjust for our own code
+                },1200)
+            })
+})
+}
